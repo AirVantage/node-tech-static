1ef1d1d7d77b14bcb24916b518c4d09dc5394c32
Jacques LESCOT <jacques.lescot@gmail.com>
2016-08-04 18:25:21 +0200

    PLTFRS-5532 - Use access token for GitHub release

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,8 +27,7 @@ module.exports = function(grunt) {
                 afterRelease: ["tag"],
                 github: {
                     repo: "AirVantage/node-tech-static",
-                    usernameVar: "GITHUB_USERNAME",
-                    passwordVar: "GITHUB_PASSWORD"
+                    accessTokenVar: "GITHUB_ACCESS_TOKEN"
                 }
             }
         },
