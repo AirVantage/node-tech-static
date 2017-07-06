1e9a2bdced3f57b5383f97ef468190b2d61881af
sgaestel <sgaestel@sierrawireless.com>
2017-07-06 12:02:18 +0200

    PLTFRS-8006 - Format with prettier using 2 spaces tabs

--- techStatic.js
+++ techStatic.js
@@ -22,43 +22,43 @@ var ls = require('list-directory-contents');
  *            
  */
 function configure(options) {
-    var app = options.app,
-        configuration = options.configuration,
-        dirs = options.staticResourcesDirs,
-        bundles = options.i18nBundles;
+  var app = options.app,
+    configuration = options.configuration,
+    dirs = options.staticResourcesDirs,
+    bundles = options.i18nBundles;
 
-    logger.debug('[tech-static] Configuring static resources with configuration');
+  logger.debug('[tech-static] Configuring static resources with configuration');
 
-    checkOptimizedDir(dirs.appResources, configuration);
+  checkOptimizedDir(dirs.appResources, configuration);
 
-    serveStaticAssets(app, {
-        version: configuration.AV_VERSION,
-        optimize: configuration.resources.optimize,
-        cache: configuration.resources.cache,
-        dirs: dirs
+  serveStaticAssets(app, {
+    version: configuration.AV_VERSION,
+    optimize: configuration.resources.optimize,
+    cache: configuration.resources.cache,
+    dirs: dirs
+  });
+
+  if (bundles) {
+    serveI18nBundles(app, {
+      version: configuration.AV_VERSION,
+      contextUrl: configuration.contextUrl,
+      bundles: bundles
     });
-
-    if (bundles) {
-        serveI18nBundles(app, {
-            version: configuration.AV_VERSION,
-            contextUrl: configuration.contextUrl,
-            bundles: bundles
-        });
-    }
+  }
 }
 
 function checkOptimizedDir(dirname, configuration) {
-    if (configuration.resources.optimize) {
-        fs.exists(path.join(dirname, 'dist', 'public'), function(exists) {
-            if (!exists) {
-                throw new Error(
-                    'Trying to launch server with optimized resources, but dist/public does not exts ; run grunt dist'
-                );
-            }
-        });
-    } else {
-        logger.warn('Will use non-optimized resources');
-    }
+  if (configuration.resources.optimize) {
+    fs.exists(path.join(dirname, 'dist', 'public'), function(exists) {
+      if (!exists) {
+        throw new Error(
+          'Trying to launch server with optimized resources, but dist/public does not exts ; run grunt dist'
+        );
+      }
+    });
+  } else {
+    logger.warn('Will use non-optimized resources');
+  }
 }
 
 /**
@@ -84,61 +84,61 @@ function checkOptimizedDir(dirname, configuration) {
  *            This argument should only be used for testing.
  */
 var serveStaticAssets = function(app, opts) {
-    logger.debug('[tech-static] Opts', opts);
+  logger.debug('[tech-static] Opts', opts);
 
-    assert.ok(opts.dirs);
-    assert.ok(opts.version);
+  assert.ok(opts.dirs);
+  assert.ok(opts.version);
 
-    var resourcesUrlBase = '/resources/' + opts.version;
-    // In case the 'debug=true' mode is activated, the URL contains the '/resources-debug/' path segment
-    // and resources should be served from the 'public' folder with no caching (maxAge: 0)
-    var resourcesDebugUrlBase = '/resources-debug/' + opts.version;
+  var resourcesUrlBase = '/resources/' + opts.version;
+  // In case the 'debug=true' mode is activated, the URL contains the '/resources-debug/' path segment
+  // and resources should be served from the 'public' folder with no caching (maxAge: 0)
+  var resourcesDebugUrlBase = '/resources-debug/' + opts.version;
 
-    var dirs = opts.dirs;
-    var optimize = opts.optimize;
-    var expressStaticCacheOptions = toExpressStaticCacheOptions(opts.cache);
-    var staticMw = opts.staticMw || _testing.staticMw || express.static;
+  var dirs = opts.dirs;
+  var optimize = opts.optimize;
+  var expressStaticCacheOptions = toExpressStaticCacheOptions(opts.cache);
+  var staticMw = opts.staticMw || _testing.staticMw || express.static;
 
-    logger.debug('[tech-static] Express Cache Options', expressStaticCacheOptions);
-    logger.debug('[tech-static] StaticPrefix:', resourcesUrlBase);
-    logger.debug('[tech-static] StaticPrefix for debug:', resourcesDebugUrlBase);
+  logger.debug('[tech-static] Express Cache Options', expressStaticCacheOptions);
+  logger.debug('[tech-static] StaticPrefix:', resourcesUrlBase);
+  logger.debug('[tech-static] StaticPrefix for debug:', resourcesDebugUrlBase);
 
-    if (optimize) {
-        var distDir = path.join(dirs.appResources, 'dist', 'public');
-        logger.debug('[tech-static] Serving optimized resources ' + resourcesUrlBase + ' from folder:', distDir);
-        printDir(distDir);
+  if (optimize) {
+    var distDir = path.join(dirs.appResources, 'dist', 'public');
+    logger.debug('[tech-static] Serving optimized resources ' + resourcesUrlBase + ' from folder:', distDir);
+    printDir(distDir);
 
-        app.use(resourcesUrlBase, staticMw(distDir, expressStaticCacheOptions));
+    app.use(resourcesUrlBase, staticMw(distDir, expressStaticCacheOptions));
 
-        _.forEach(dirs, function(dir) {
-            var publicDir = path.join(dir, 'public');
-            logger.debug(
-                '[tech-static] Serving non optimized  resources ' + resourcesDebugUrlBase + ' from folder:',
-                publicDir
-            );
-            printDir(publicDir);
-            app.use(
-                resourcesDebugUrlBase,
-                staticMw(publicDir, {
-                    maxAge: 0
-                })
-            );
-        });
+    _.forEach(dirs, function(dir) {
+      var publicDir = path.join(dir, 'public');
+      logger.debug(
+        '[tech-static] Serving non optimized  resources ' + resourcesDebugUrlBase + ' from folder:',
+        publicDir
+      );
+      printDir(publicDir);
+      app.use(
+        resourcesDebugUrlBase,
+        staticMw(publicDir, {
+          maxAge: 0
+        })
+      );
+    });
 
-        // Specific routing for the all.css file which is always retrieved from the dist/public/css folder
-        var cssDir = path.join(distDir, 'css');
-        logger.debug(
-            '[tech-static] Serving all.css file for debug url ' + resourcesDebugUrlBase + '/css from folder ' + cssDir
-        );
-        printDir(cssDir);
-        app.use(resourcesDebugUrlBase + '/css', staticMw(cssDir, expressStaticCacheOptions));
-    } else {
-        _.forEach(dirs, function(dir) {
-            var publicDir = path.join(dir, 'public');
-            logger.debug('[tech-static] Serving non optimized resources from folder:', publicDir);
-            app.use(resourcesUrlBase, staticMw(publicDir, expressStaticCacheOptions));
-        });
-    }
+    // Specific routing for the all.css file which is always retrieved from the dist/public/css folder
+    var cssDir = path.join(distDir, 'css');
+    logger.debug(
+      '[tech-static] Serving all.css file for debug url ' + resourcesDebugUrlBase + '/css from folder ' + cssDir
+    );
+    printDir(cssDir);
+    app.use(resourcesDebugUrlBase + '/css', staticMw(cssDir, expressStaticCacheOptions));
+  } else {
+    _.forEach(dirs, function(dir) {
+      var publicDir = path.join(dir, 'public');
+      logger.debug('[tech-static] Serving non optimized resources from folder:', publicDir);
+      app.use(resourcesUrlBase, staticMw(publicDir, expressStaticCacheOptions));
+    });
+  }
 };
 
 /**
@@ -150,34 +150,34 @@ var serveStaticAssets = function(app, opts) {
  * @options []cacheConfiguration.seconds]
  */
 function toExpressStaticCacheOptions(cacheConfiguration) {
-    logger.debug('[tech-static] computing cache options', cacheConfiguration);
+  logger.debug('[tech-static] computing cache options', cacheConfiguration);
 
-    var res = {
-        maxAge: 0
-    };
-    if (cacheConfiguration) {
-        if (cacheConfiguration.seconds) {
-            res = {
-                maxAge: cacheConfiguration.seconds * 1000
-            };
-        }
-        if (cacheConfiguration.ms) {
-            res = {
-                maxAge: cacheConfiguration.ms
-            };
-        }
+  var res = {
+    maxAge: 0
+  };
+  if (cacheConfiguration) {
+    if (cacheConfiguration.seconds) {
+      res = {
+        maxAge: cacheConfiguration.seconds * 1000
+      };
     }
-    return res;
+    if (cacheConfiguration.ms) {
+      res = {
+        maxAge: cacheConfiguration.ms
+      };
+    }
+  }
+  return res;
 }
 
 function printDir(dirname) {
-    ls(dirname, function(err, tree) {
-        if (err) {
-            logger.warn('[tech-static] Error while listing dir ' + dirname + ' : ' + JSON.stringify(err));
-        } else {
-            logger.debug('[tech-static] Counted ' + _.size(tree) + ' elements in dir ' + dirname);
-        }
-    });
+  ls(dirname, function(err, tree) {
+    if (err) {
+      logger.warn('[tech-static] Error while listing dir ' + dirname + ' : ' + JSON.stringify(err));
+    } else {
+      logger.debug('[tech-static] Counted ' + _.size(tree) + ' elements in dir ' + dirname);
+    }
+  });
 }
 
 /**
@@ -198,32 +198,32 @@ function printDir(dirname) {
  *          {String} base of all *other* urls (eg "/" or "")
  */
 var serveI18nBundles = function(app, opts) {
-    assert.ok(opts.bundles);
-    assert.ok(opts.version);
+  assert.ok(opts.bundles);
+  assert.ok(opts.version);
 
-    var resourcesUrlBase = '/resources/' + opts.version;
-    var resourcesDebugUrlBase = '/resources-debug/' + opts.version;
+  var resourcesUrlBase = '/resources/' + opts.version;
+  var resourcesDebugUrlBase = '/resources-debug/' + opts.version;
 
-    var bundles = opts.bundles;
+  var bundles = opts.bundles;
 
-    [resourcesUrlBase, resourcesDebugUrlBase].forEach(function(urlBase) {
-        bundles.forEach(function(bundle) {
-            var en_path = [urlBase, '/i18n/', bundle, '_en.properties'].join('');
-            var no_locale_path = [urlBase, '/i18n/', bundle, '.properties'].join('');
-            app.use(en_path, function(req, res) {
-                res.redirect(opts.contextUrl + no_locale_path);
-            });
-        });
+  [resourcesUrlBase, resourcesDebugUrlBase].forEach(function(urlBase) {
+    bundles.forEach(function(bundle) {
+      var en_path = [urlBase, '/i18n/', bundle, '_en.properties'].join('');
+      var no_locale_path = [urlBase, '/i18n/', bundle, '.properties'].join('');
+      app.use(en_path, function(req, res) {
+        res.redirect(opts.contextUrl + no_locale_path);
+      });
     });
+  });
 };
 
 var _testing = {
-    serveStaticAssets: serveStaticAssets,
-    serveI18nBundles: serveI18nBundles,
-    checkOptimizedDir: checkOptimizedDir
+  serveStaticAssets: serveStaticAssets,
+  serveI18nBundles: serveI18nBundles,
+  checkOptimizedDir: checkOptimizedDir
 };
 
 module.exports = {
-    configure: configure,
-    _testing: _testing
+  configure: configure,
+  _testing: _testing
 };
--- test/techStaticSpec.js
+++ test/techStaticSpec.js
@@ -7,165 +7,165 @@ var path = require('path');
 logger.setup({});
 
 describe('node-tech-static', function() {
-    var app,
-        opts = null;
+  var app,
+    opts = null;
 
+  beforeEach(function() {
+    app = {
+      use: sinon.spy()
+    };
+  });
+
+  describe('#configure', function() {
+    it('Uses cache options from configuration', function() {
+      var configuration = {
+        AV_VERSION: '1.0',
+        resources: {
+          cache: {
+            seconds: 15
+          }
+        }
+      };
+
+      techStatic._testing.staticMw = function(folder, options) {
+        return [folder.split(path.sep).join('/'), options];
+      };
+      techStatic.configure(app, configuration, ['dir1'], []);
+
+      sinon.assert.calledWith(app.use, '/resources/1.0', [
+        'dir1/public',
+        {
+          maxAge: 15000
+        }
+      ]);
+    });
+  });
+
+  describe('#serveStaticAssets', function() {
     beforeEach(function() {
-        app = {
-            use: sinon.spy()
-        };
+      opts = {
+        cache: {
+          ms: 123456
+        },
+        version: '1.0',
+        optimize: false,
+        dirs: ['.', '../../node-ui-commons'],
+        staticMw: function(folder, options) {
+          return [folder.split(path.sep).join('/'), options];
+        }
+      };
     });
 
-    describe('#configure', function() {
-        it('Uses cache options from configuration', function() {
-            var configuration = {
-                AV_VERSION: '1.0',
-                resources: {
-                    cache: {
-                        seconds: 15
-                    }
-                }
-            };
+    it('Serves from a list of dirs', function() {
+      techStatic._testing.serveStaticAssets(app, opts);
 
-            techStatic._testing.staticMw = function(folder, options) {
-                return [folder.split(path.sep).join('/'), options];
-            };
-            techStatic.configure(app, configuration, ['dir1'], []);
-
-            sinon.assert.calledWith(app.use, '/resources/1.0', [
-                'dir1/public',
-                {
-                    maxAge: 15000
-                }
-            ]);
-        });
+      sinon.assert.calledWith(app.use, '/resources/1.0', [
+        'public',
+        {
+          maxAge: 123456
+        }
+      ]);
+      sinon.assert.calledWith(app.use, '/resources/1.0', [
+        '../../node-ui-commons/public',
+        {
+          maxAge: 123456
+        }
+      ]);
     });
 
-    describe('#serveStaticAssets', function() {
-        beforeEach(function() {
-            opts = {
-                cache: {
-                    ms: 123456
-                },
-                version: '1.0',
-                optimize: false,
-                dirs: ['.', '../../node-ui-commons'],
-                staticMw: function(folder, options) {
-                    return [folder.split(path.sep).join('/'), options];
-                }
-            };
-        });
+    it('Serves optimized resource from a single dir', function() {
+      opts.optimize = true;
 
-        it('Serves from a list of dirs', function() {
-            techStatic._testing.serveStaticAssets(app, opts);
+      techStatic._testing.serveStaticAssets(app, opts);
 
-            sinon.assert.calledWith(app.use, '/resources/1.0', [
-                'public',
-                {
-                    maxAge: 123456
-                }
-            ]);
-            sinon.assert.calledWith(app.use, '/resources/1.0', [
-                '../../node-ui-commons/public',
-                {
-                    maxAge: 123456
-                }
-            ]);
-        });
-
-        it('Serves optimized resource from a single dir', function() {
-            opts.optimize = true;
-
-            techStatic._testing.serveStaticAssets(app, opts);
-
-            sinon.assert.calledWith(app.use, '/resources/1.0', [
-                'dist/public',
-                {
-                    maxAge: 123456
-                }
-            ]);
-        });
-
-        it("Serves non optimized resources from folders suffixed with '-debug' when using 'debug=true'", function() {
-            opts.optimize = true;
-
-            techStatic._testing.serveStaticAssets(app, opts);
-
-            sinon.assert.calledWith(app.use, '/resources-debug/1.0', [
-                'public',
-                {
-                    maxAge: 0
-                }
-            ]);
-            sinon.assert.calledWith(app.use, '/resources-debug/1.0', [
-                '../../node-ui-commons/public',
-                {
-                    maxAge: 0
-                }
-            ]);
-        });
-
-        it('converts seconds to ms', function() {
-            opts.cache.seconds = 1;
-            delete opts.cache.ms;
-
-            techStatic._testing.serveStaticAssets(app, opts);
-
-            sinon.assert.calledWith(app.use, '/resources/1.0', [
-                'public',
-                {
-                    maxAge: 1000
-                }
-            ]);
-        });
+      sinon.assert.calledWith(app.use, '/resources/1.0', [
+        'dist/public',
+        {
+          maxAge: 123456
+        }
+      ]);
     });
 
-    describe('#serverI18nBundles', function() {
-        it('Adds default service for all bundles', function() {
-            techStatic._testing.serveI18nBundles(app, {
-                bundles: ['Portal', 'Error'],
-                version: '1.0',
-                contextUrl: '/portal'
-            });
+    it("Serves non optimized resources from folders suffixed with '-debug' when using 'debug=true'", function() {
+      opts.optimize = true;
 
-            sinon.assert.calledWith(app.use, '/resources/1.0/i18n/Portal_en.properties', sinon.match.func);
-            sinon.assert.calledWith(app.use, '/resources/1.0/i18n/Portal_en.properties', sinon.match.func);
-        });
+      techStatic._testing.serveStaticAssets(app, opts);
 
-        it('Redirects en to default property files', function() {
-            techStatic._testing.serveI18nBundles(app, {
-                bundles: ['Portal'],
-                version: '1.0',
-                contextUrl: '/portal'
-            });
-
-            var spyCall = app.use.firstCall;
-            var handler = spyCall.args[1];
-
-            var res = {
-                redirect: sinon.spy()
-            };
-            handler(null, res);
-            sinon.assert.calledOnce(res.redirect);
-            sinon.assert.calledWith(res.redirect, '/portal/resources/1.0/i18n/Portal.properties');
-        });
-
-        it("Redirects en to default for 'debug' resources", function() {
-            techStatic._testing.serveI18nBundles(app, {
-                bundles: ['Portal'],
-                version: '1.0',
-                contextUrl: '/portal'
-            });
-
-            var spyCall = app.use.getCall(1);
-            var handler = spyCall.args[1];
-
-            var res = {
-                redirect: sinon.spy()
-            };
-            handler(null, res);
-            sinon.assert.calledOnce(res.redirect);
-            sinon.assert.calledWith(res.redirect, '/portal/resources-debug/1.0/i18n/Portal.properties');
-        });
+      sinon.assert.calledWith(app.use, '/resources-debug/1.0', [
+        'public',
+        {
+          maxAge: 0
+        }
+      ]);
+      sinon.assert.calledWith(app.use, '/resources-debug/1.0', [
+        '../../node-ui-commons/public',
+        {
+          maxAge: 0
+        }
+      ]);
     });
+
+    it('converts seconds to ms', function() {
+      opts.cache.seconds = 1;
+      delete opts.cache.ms;
+
+      techStatic._testing.serveStaticAssets(app, opts);
+
+      sinon.assert.calledWith(app.use, '/resources/1.0', [
+        'public',
+        {
+          maxAge: 1000
+        }
+      ]);
+    });
+  });
+
+  describe('#serverI18nBundles', function() {
+    it('Adds default service for all bundles', function() {
+      techStatic._testing.serveI18nBundles(app, {
+        bundles: ['Portal', 'Error'],
+        version: '1.0',
+        contextUrl: '/portal'
+      });
+
+      sinon.assert.calledWith(app.use, '/resources/1.0/i18n/Portal_en.properties', sinon.match.func);
+      sinon.assert.calledWith(app.use, '/resources/1.0/i18n/Portal_en.properties', sinon.match.func);
+    });
+
+    it('Redirects en to default property files', function() {
+      techStatic._testing.serveI18nBundles(app, {
+        bundles: ['Portal'],
+        version: '1.0',
+        contextUrl: '/portal'
+      });
+
+      var spyCall = app.use.firstCall;
+      var handler = spyCall.args[1];
+
+      var res = {
+        redirect: sinon.spy()
+      };
+      handler(null, res);
+      sinon.assert.calledOnce(res.redirect);
+      sinon.assert.calledWith(res.redirect, '/portal/resources/1.0/i18n/Portal.properties');
+    });
+
+    it("Redirects en to default for 'debug' resources", function() {
+      techStatic._testing.serveI18nBundles(app, {
+        bundles: ['Portal'],
+        version: '1.0',
+        contextUrl: '/portal'
+      });
+
+      var spyCall = app.use.getCall(1);
+      var handler = spyCall.args[1];
+
+      var res = {
+        redirect: sinon.spy()
+      };
+      handler(null, res);
+      sinon.assert.calledOnce(res.redirect);
+      sinon.assert.calledWith(res.redirect, '/portal/resources-debug/1.0/i18n/Portal.properties');
+    });
+  });
 });
