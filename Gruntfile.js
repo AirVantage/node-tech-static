da980e7f915163087d09970f8d1b69ce0958c64b
Simon Gaestel <simon.gaestel+git@gmail.com>
2017-07-24 16:52:09 +0200

    PLTFRS-8006 - Format with prettier (#8)
    * PLTFRS-8006 - Use prettier as formatter and eslint as linter
    
    * PLTFRS-8006 - Format with prettier using 2 spaces tabs
    
    * PLTFRS-8006 - Make one line when possible
    
    * PLTFRS-8006 - Updates following review

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,42 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-tag');
 
-    grunt.loadNpmTasks("grunt-mocha-test");
-    grunt.loadNpmTasks("grunt-release");
-    grunt.loadNpmTasks("grunt-tag");
-
-    grunt.initConfig({
-        mochaTest: {
-            test_spec: {
-                options: {
-                    reporter: "spec"
-                },
-                src: ["test/**/*.js"]
-            },
-            test_json: {
-                options: {
-                    reporter: "json",
-                    quiet: true,
-                    captureFile: "reports/json/TEST-tech-static.json"
-                },
-                src: ["test/**/*.js"]
-            }
+  grunt.initConfig({
+    mochaTest: {
+      test_spec: {
+        options: {
+          reporter: 'spec'
         },
-        release: {
-            options: {
-                npm: false,
-                afterRelease: ["tag"],
-                github: {
-                    repo: "AirVantage/node-tech-static",
-                    accessTokenVar: "GITHUB_ACCESS_TOKEN"
-                }
-            }
+        src: ['test/**/*.js']
+      },
+      test_json: {
+        options: {
+          reporter: 'json',
+          quiet: true,
+          captureFile: 'reports/json/TEST-tech-static.json'
         },
-        tag: {
-            options: {
-                tagName: "<%= version.match(/\\d*/) %>.x"
-            }
+        src: ['test/**/*.js']
+      }
+    },
+    release: {
+      options: {
+        npm: false,
+        afterRelease: ['tag'],
+        github: {
+          repo: 'AirVantage/node-tech-static',
+          accessTokenVar: 'GITHUB_ACCESS_TOKEN'
         }
-    });
+      }
+    },
+    tag: {
+      options: {
+        tagName: '<%= version.match(/\\d*/) %>.x'
+      }
+    }
+  });
 
-    grunt.registerTask("test", ["mochaTest:test_spec"]);
-    grunt.registerTask("test-json", ["mochaTest:test_json"]);
-
+  grunt.registerTask('test', ['mochaTest:test_spec']);
+  grunt.registerTask('test-json', ['mochaTest:test_json']);
 };
